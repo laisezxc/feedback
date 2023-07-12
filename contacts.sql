e40a3ff76e1111475366115d8105dd64b96e090f
Evgenii <eugene.rose@mail.ru>
2023-07-12 14:13:36 +0300

    Update contacts.sql

--- contacts.sql
+++ contacts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 10 2023 г., 12:43
+-- Время создания: Июл 12 2023 г., 14:07
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `database_name`
+-- База данных: `test`
 --
 
 -- --------------------------------------------------------
@@ -28,32 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `contacts` (
-  `user_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_name` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(11) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_message` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `contacts`
 --
 
 INSERT INTO `contacts` (`user_name`, `phone`, `email`, `user_message`) VALUES
-('sdgwdwg', '1313131', 'eugeneabramov@mail.ru', 'sdfghsdg'),
-('', '', '', ''),
-('sdghdsgf', '12312314', 'eugeneabramov@mail.ru', '123123'),
-('', '', '', ''),
-('sdghdsgf', '12312314', 'eugeneabramov@mail.ru', '123123'),
-('', '', '', ''),
-('wqeqe', '32131', 'eugeneabramov@mail.ru', 'sdfgsdfg'),
-('', '', '', ''),
-('wqeqe', '32131', 'eugeneabramov@mail.ru', 'sdfgsdfg'),
-('', '', '', ''),
-('wqeqe', '32131', 'eugeneabramov@mail.ru', 'sdfgsdfg'),
-('', '', '', ''),
-('wqeqe', '32131', 'eugeneabramov@mail.ru', 'sdfgsdfg'),
-('', '', '', ''),
-('dfgsdg', '123124141', '3123123dfg@mail.ru', 'sdfsdf');
+('dgsdg', '13123', 'gdgdsgdsg@mail.ru', 'safdasfas');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
